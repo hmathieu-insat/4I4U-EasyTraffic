47895f9c8528f633f0d66e10f7657f817a4811d7
Nicolas Perruisset <perruisset@insa-toulouse.fr>
2022-11-16 18:02:00 +0100

    ajout classe Feu

---
+++ 4I4U-LiteTrak/Model/Feu.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _4I4U_LiteTrak.Model
+{
+    public class Feu
+    {
+        private int InfoTraffic { get; set; }
+        private int TempsAjout { get; set; }
+        private bool DoitClignoter { get; set; }
+        private List<Feu> FeuxAval { get; set; } = new();
+
+
+    }
+}
