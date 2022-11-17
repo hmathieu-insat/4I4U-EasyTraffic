17199fd6a604bea95fc380f00bded441a2372279
Nicolas Perruisset <perruisset@insa-toulouse.fr>
2022-11-17 10:27:17 +0100

    service doitclignoter

---
+++ 4I4U-LiteTrak/Services/Clignotement.cs
@@ -0,0 +1,43 @@
+﻿using _4I4U_LiteTrak.Model;
+
+namespace _4I4U_LiteTrak.Services
+{
+    public class Clignotement
+    {
+        public List<Feu> listeFeux { get; private set; }
+        public Clignotement(List<Feu> liste)
+        {
+            listeFeux = liste;
+        }
+        public List<int> recupererInfoTraffic(List<Feu> Feux)
+        {
+            List<int> ListInfoTraffic = new();
+            foreach (Feu Feu in Feux)
+            {
+                ListInfoTraffic.Add(Feu.InfoTraffic);
+
+            }
+            return ListInfoTraffic;
+
+        }
+        public bool calculDoitClignoter(ListInfoTraffic)
+        {
+                public bool DoitClignoter = 0;
+                public int count = 0;
+                List<int> ListInfoTraffic = recupererInfoTraffic(listeFeux);
+                foreach (value in ListInfoTraffic) {
+                    if (ListeInfoTraffic(value) > 5) {
+                        count++;
+                }
+                if (count > 2) {
+                    DoitClignoter = 1;
+                } else
+                {
+                    DoitClignoter = 0;
+                }
+                return DoitClignoter;
+
+            }
+
+    }
+}
